9d9d65306f341e862f4c4632ec5d353cf2bef1a0
Kyle Walsh <kew96@cornell.edu>
2020-11-05 13:39:28 -0500

    Reformat

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 
 INDEX_TICKERS = {
     'JNJ', 'UNH', 'PFE', 'MRK', 'TMO', 'ABT',  # US Healthcare
-    'FB', 'GOOGL', 'GOOG', 'NFLX', 'TMUS', 'CHTR',  # US Communications  TODO: Only NFLX reaches back to 2000
-    'AAPL', 'MSFT', 'NVDA', 'V', 'MA', 'CRM', 'PYPL',  # US Technology TODO: Only AAPL, MSFT, NVDA reach back to 2000
+    'FB', 'GOOGL', 'GOOG', 'NFLX', 'TMUS', 'CHTR',  # US Communications  TODO: FB, GOOGL, GOOG, TMUS, CHTR
+    'AAPL', 'MSFT', 'NVDA', 'V', 'MA', 'CRM', 'PYPL',  # US Technology TODO: V, MA, CRM
     'AMZN', 'HD', 'MCD', 'NKE', 'LOW', 'SBUX', 'TGT',  # US Consumer Discretionary
-    'AMT', 'PLD', 'CCI', 'EQIX', 'DLR', 'PSA', 'SBAC', 'WELL',  # US Real Estate TODO: EQIX, DLR do not go to 2000
-    'CVX', 'XOM', 'KMI', 'WMB', 'COP', 'SLB', 'MPC', 'PSX',  # US Energy TODO: KMI, MPC, PSX do not go to 2000
-    'PG', 'WMT', 'KO', 'PEP', 'COST', 'MDLZ', 'CL', 'MO',  # US Consumer Staples TODO: MDLX does not go to 2000
-    'UNP', 'HON', 'UPS', 'MMM', 'CAT', 'LMT', 'RTX', 'BA',  # US Industrials TODO: KMI, MPC, PSX do not go to 2000
+    'AMT', 'PLD', 'CCI', 'EQIX', 'DLR', 'PSA', 'SBAC', 'WELL',  # US Real Estate TODO: EQIX, DLR
+    'CVX', 'XOM', 'KMI', 'WMB', 'COP', 'SLB', 'MPC', 'PSX',  # US Energy TODO: KMI, MPC, PSX
+    'PG', 'WMT', 'KO', 'PEP', 'COST', 'MDLZ', 'CL', 'MO',  # US Consumer Staples TODO: MDLX
+    'UNP', 'HON', 'UPS', 'MMM', 'CAT', 'LMT', 'RTX', 'BA',  # US Industrials TODO: KMI, MPC, PSX
     'BRK.B', 'JPM', 'BAC', 'WFC', 'C', 'BLK', 'SPGI', 'MS',  # US Financials
     'NEE', 'DUK', 'D', 'SO', 'AEP', 'EXC', 'XEL', 'SRE',  # US Utilities
-    'LIN', 'APD', 'SHW', 'NEM', 'ECL', 'DD', 'PPG', 'DOW',  # US Materials TODO: DOW does not go to 2000
+    'LIN', 'APD', 'SHW', 'NEM', 'ECL', 'DD', 'PPG', 'DOW',  # US Materials TODO: DOW
     }
