514affc50ed8abc305b6b749a35421f355a0f3c2
Kyle Walsh <kew96@cornell.edu>
2020-11-05 13:31:49 -0500

    Check historical data

--- index.py
+++ index.py
@@ -1,17 +1,14 @@
-import pandas as pd
-
-# TODO: Check historical data
 
 INDEX_TICKERS = {
     'JNJ', 'UNH', 'PFE', 'MRK', 'TMO', 'ABT',  # US Healthcare
-    'FB', 'GOOGL', 'GOOG', 'NFLX', 'TMUS', 'CHTR',  # US Communications
-    'AAPL', 'MSFT', 'NVDA', 'V', 'MA', 'CRM', 'PYPL',  # US Technology
+    'FB', 'GOOGL', 'GOOG', 'NFLX', 'TMUS', 'CHTR',  # US Communications  TODO: Only NFLX reaches back to 2000
+    'AAPL', 'MSFT', 'NVDA', 'V', 'MA', 'CRM', 'PYPL',  # US Technology TODO: Only AAPL, MSFT, NVDA reach back to 2000
     'AMZN', 'HD', 'MCD', 'NKE', 'LOW', 'SBUX', 'TGT',  # US Consumer Discretionary
-    'AMT', 'PLD', 'CCI', 'EQIX', 'DLR', 'PSA', 'SBAC', 'WELL',  # US Real Estate
-    'CVX', 'XOM', 'KMI', 'WMB', 'COP', 'SLB', 'MPC', 'PSX',  # US Energy
-    'PG', 'WMT', 'KO', 'PEP', 'COST', 'MDLZ', 'CL', 'MO',  # US Consumer Staples
-    'UNP', 'HON', 'UPS', 'MMM', 'CAT', 'LMT', 'RTX', 'BA',  # US Industrials
+    'AMT', 'PLD', 'CCI', 'EQIX', 'DLR', 'PSA', 'SBAC', 'WELL',  # US Real Estate TODO: EQIX, DLR do not go to 2000
+    'CVX', 'XOM', 'KMI', 'WMB', 'COP', 'SLB', 'MPC', 'PSX',  # US Energy TODO: KMI, MPC, PSX do not go to 2000
+    'PG', 'WMT', 'KO', 'PEP', 'COST', 'MDLZ', 'CL', 'MO',  # US Consumer Staples TODO: MDLX does not go to 2000
+    'UNP', 'HON', 'UPS', 'MMM', 'CAT', 'LMT', 'RTX', 'BA',  # US Industrials TODO: KMI, MPC, PSX do not go to 2000
     'BRK.B', 'JPM', 'BAC', 'WFC', 'C', 'BLK', 'SPGI', 'MS',  # US Financials
     'NEE', 'DUK', 'D', 'SO', 'AEP', 'EXC', 'XEL', 'SRE',  # US Utilities
-    'LIN', 'APD', 'SHW', 'NEM', 'ECL', 'DD', 'PPG', 'DOW',  # US Materials
+    'LIN', 'APD', 'SHW', 'NEM', 'ECL', 'DD', 'PPG', 'DOW',  # US Materials TODO: DOW does not go to 2000
     }
